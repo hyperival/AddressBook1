12f6b7872939b283ee1fb3f0482895e2166a2419
Hyperival <sanjaygautam779@gmail.com>
2022-11-24 15:11:03 +0530

    create a Contacts in Address Book with first and last names, address, city, state,

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -9,9 +9,40 @@ namespace AddressBook
 {
     internal class AddressBookMain
     {
-        static void Main(string[] args)
+        internal class Contact
         {
+            public string First_Name { get; set; }
+            public string Last_Name { get; set; }
+            public string Present_Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Email_Id { get; set; }
+            public string Phone_Number { get; set; }
+            public string Zip { get; set; }
+        }
+        public static void Main(string[] args)
+        {
+            List<Contact> Addressbook = new List<Contact>();
+            Contact contact = new Contact();
             Console.WriteLine("Welcome to Address Book Program");
+            Console.WriteLine("Add a New Contact:");
+            Console.WriteLine("Enter your first_name");
+            contact.First_Name = Console.ReadLine();
+            Console.WriteLine("Enter your last_name");
+            contact.Last_Name = Console.ReadLine();
+            Console.WriteLine("Enter your Present_address");
+            contact.Present_Address = Console.ReadLine();
+            Console.WriteLine("Enter your City");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter your State");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter your Email_Id");
+            contact.Email_Id = Console.ReadLine();
+            Console.WriteLine("Enter your Phone_Number");
+            contact.Phone_Number = Console.ReadLine();
+            Console.WriteLine("Enter your ZipCode");
+            contact.Zip = Console.ReadLine();
+            Addressbook.Add(contact);
         }
     }
 }
